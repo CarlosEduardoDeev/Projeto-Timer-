8ae7d70925660c9c9f37274ded407d00b5803f62
Carlos Eduardo <deev.carloseduardo@gmail.com>
2022-07-27 17:48:09 -0300

    Codigo subido

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './Components/Button'
+import { Router } from './Components/Router'
 import { GlobalStyle } from './Styles/global'
 import { defaultTheme } from './Styles/Themes/default'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router/>
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
---
+++ src/Components/Router.tsx
@@ -0,0 +1,12 @@
+import { Route,Routes } from 'react-router-dom'
+import { Home } from '../Pages/Home'
+import { History } from '../Pages/History'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path="/History" element={<History />}/>
+    </Routes>
+  )
+}
